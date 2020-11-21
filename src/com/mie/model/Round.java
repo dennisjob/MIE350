488b778fae1e3cb11f5261c34615af57a4a9fc27
Lara Caglar <caglarla@WIN.ecf.utoronto.ca>
2020-11-20 22:03:09 -0500

    This commit adds a RoundDao class which represents the Assessment table in the database. Also adds changes to the Round class (setters, toString, constructor method

--- src/com/mie/model/Round.java
+++ src/com/mie/model/Round.java
@@ -13,6 +13,8 @@ public class Round {
 	private String location;
 	private String assessorName;
 	
+	public Round() {  }
+	
 	public Round(int roundId, int appId, int companyId, int userId, Timestamp dateTime, 
 			String roundType, String location, String assessorName) {
 		this.roundId = roundId;
@@ -55,6 +57,46 @@ public class Round {
 	
 	public String getAssessorName() {
 		return assessorName;
+	}
+	
+	public void setRoundId(int roundId) {
+		this.roundId = roundId;
+	}
+	
+	public void setAppId(int appId) {
+		this.appId = appId;
+	}
+	
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public void setDateTime(Timestamp dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	public void setRoundType(String roundType) {
+		this.roundType = roundType;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public void setAssessorName(String assessorName) {
+		this.assessorName = assessorName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Assessment [roundId=" + roundId + ", appId=" + appId + ", companyId="
+				+ companyId + ", userId=" + userId + ", dateTime=" + dateTime
+				+ ", roundType" + roundType + ", location=" + location + ", assessorName"
+				+ assessorName + "]";
 	}
 
 }
